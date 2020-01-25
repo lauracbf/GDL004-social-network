b4608ebf6bb1a3e256f4244afacea80c09eb4c58
lauracbf <53447565+lauracbf@users.noreply.github.com>
2020-01-25 16:10:39 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,25 +9,12 @@ window.addEventListener('load', initMain);
 
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyA87a5JfZWcgDb_HlHIKR5-TDIF8i52uaI",
-    authDomain: "fir-usuarios-3375c.firebaseapp.com",
-    databaseURL: "https://fir-usuarios-3375c.firebaseio.com",
-    projectId: "fir-usuarios-3375c",
-    storageBucket: "fir-usuarios-3375c.appspot.com",
-    messagingSenderId: "772408234103",
-    appId: "1:772408234103:web:bdae0a46a8d99f9f177672"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore through Firebase
-// firebase.initializeApp({
-//     apiKey: '"AIzaSyA87a5JfZWcgDb_HlHIKR5-TDIF8i52uaI"',
-//     authDomain: 'fir-usuarios-3375c.firebaseapp.com',
-//     projectId: 'fir-usuarios-3375c'
-//   });
+
   
   var db = firebase.firestore();
 
