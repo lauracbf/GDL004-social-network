c41632a216ca638b7a5e2d883470bee31bba674a
GeorginaPQ <giorginapqe@gmail.com>
2020-01-30 02:04:01 -0600

    Añadiendo estilos a formulario de registro/incio y retocando algunos detalles del formulario de inicio de sesion

--- src/views/pages/signin.js
+++ src/views/pages/signin.js
@@ -1,31 +1,37 @@
 export default() => {
     const signIn = `
-        <h1 class="mainHeader">Our<i class="fab fa-suse"></i>Pets</h1>
+        <main class="main-container"></main>
+        <div class="container-form-signIn">
+        <div class="form-signIn">
+        <img src="/src/images/dog-paw.svg" alt="Logo" class="foot-print"> 
+        <p class="signIn-logo">OurPets</p>
+        <p class="title f-open">Te damos la Bienvenida</p>
 
         <form class="sign-in-form" id="signInForm">
             
-                <label for="email-input">Email</label>
-                <input name="userEmail" type="email" id="email-input" placeholder="Ingresa tu E-mail..."/>
+                <label for="email-input" class="hidden">Email</label>
+                <input class="f-open" name="userEmail" type="email" id="email-input" placeholder="E-mail..."/>
         
-                <label for="password-input">Password</label>
-                <input name="userPassword" type="password" id="password-input" placeholder="Ingresa to password..."/>
+                <label for="password-input" class="hidden ">Password</label>
+                <input class="f-open" name="userPassword" type="password" id="password-input" placeholder="Password..."/>
             
-                <button id="getInfo">Sign In</button>
+                <button class="f-open cta-start submit"  id="getInfo">Sign In</button>
         
         </form> 
         
-                <p class="spacer"><i class="fas fa-bone"></i>OR<i class="fas fa-bone"></i></p>
+                <p class="spacer f-open"><i class="fas fa-bone"></i>OR<i class="fas fa-bone"></i></p>
 
-                <p class="">Sign in with:</p>
+                <p class="f-open">Sign in with:</p>
 
                 <ul class="social-sign-in">
-                    <li><button id= "button-Google"><i class="fab fa-google"></i></button></li>
+                    <li id= "button-Google"><i class="fab fa-google"></i></></li>
                     <li><i class="fab fa-github"></i></li>
                     <li><i class="fab fa-facebook-f"></i></li>
                 </ul>
                 
-                <p>Don't have an account?<a href="#/register">SIGN UP</a></p>
-       
+                <p class="f-open translate">Don't have an account?<a href="#/register"><br><small>SIGN UP</small></a></p>
+        </div>
+        </div>
     `;
 
     const divSignIn = document.createElement('div');
