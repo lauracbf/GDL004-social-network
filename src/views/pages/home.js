a7e7336121d1b7b6d76466b0b57ad67e65fb6989
GeorginaPQ <giorginapqe@gmail.com>
2020-01-29 23:25:34 -0600

    Añadiendo estilos para la pantalla de bienvenida

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -1,8 +1,17 @@
 export default() => {
     
     const homePage = `
-        <h1>HOME</h1>
-        <button><a href="#/signin">Continue</a></button>
+    <main class="main-container"></main>
+        <div class="content">
+        <div class="logo">
+            <img src="/src/images/dog-paw.svg" alt="Logo">
+            <p class="text-logo">OurPets</p>
+        </div>  
+        <h1 class="title f-open">Te damos la bienvenida a Ourpets</h1>
+        <h3 class="f-open sub-title">Encuentra nuevas ideas para los mas queridos en casa </h3>
+        <a class="f-open cta-start" href="#/signin">Conoce más</a>
+        <footer class="f-open white footer">Copyright 2020 &copy; Created by Geo, Lau y Moni.</footer>
+        </div>
     `;
 
     const divHome = document.createElement('div');
